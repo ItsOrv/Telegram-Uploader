67726db67ded0369e6eb336c17fcd22d1991ca13
ItsOrv <poriya.saw@gmail.com>
2021-01-22 09:24:45 +0330

    update main for mysql

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from telethon import TelegramClient
-import os
-from dotenv import load_dotenv
+from config.config import Config
+from handlers.admin_handlers import AdminHandlers
+from handlers.super_admin_handlers import SuperAdminHandlers
+from handlers.user_handlers import UserStart
+from handlers.start_handlers import StartHandlers
+from utils.keyboards import Keyboards
+from utils.save import Save
+from utils.send import Send
+from config.db_manager import DatabaseManager
+from config.logger_config import logger
 import asyncio
 
-load_dotenv()
-
 async def main():
-    bot = TelegramClient(
-        'bot_session',
-        int(os.getenv('API_ID')),
-        os.getenv('API_HASH')
-    )
-    await bot.start(bot_token=os.getenv('BOT_TOKEN'))
-    print("bot started")
+    config = Config()
+    bot = TelegramClient('bot_session', config.api_id, config.api_hash)
+    await bot.start(bot_token=config.bot_token)
+
+    db = DatabaseManager(config)
+
+    admin_handlers = AdminHandlers(bot, config, db)
+    super_admin_handlers = SuperAdminHandlers(bot, config, admin_handlers, db)
+    user_start = UserStart(bot, config, db)
+    start_handlers = StartHandlers(bot, config, db)
+
+    from telethon import events
+    bot.add_event_handler(start_handlers.handle_start, events.NewMessage(pattern='/start'))
+
+    logger.info("Bot started")
     await bot.run_until_disconnected()
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
