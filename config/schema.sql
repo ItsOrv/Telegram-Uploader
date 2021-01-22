c4e9478e53ee4c23c2b32c21e75bc7d7f5401b3e
ItsOrv <poriya.saw@gmail.com>
2021-01-22 14:34:29 +0330

    fix user table missing cols

--- config/schema.sql
+++ config/schema.sql
@@ -37,6 +37,9 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL UNIQUE,
     has_access BOOLEAN DEFAULT FALSE,
+    username VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
