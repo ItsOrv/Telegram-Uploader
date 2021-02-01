b8d3c54865745d88f459438a0251c56891080fb2
ItsOrv <poriya.saw@gmail.com>
2021-02-01 20:50:53 +0330

    use logger in main

--- main.py
+++ main.py
@@ -11,23 +11,40 @@ from config.db_manager import DatabaseManager
 from config.logger_config import logger
 import asyncio
 
+async def init_bot(bot, config):
+    try:
+        bot_info = await bot.get_me()
+        if not bot_info.username:
+            raise ValueError("unable to get bot username")
+        config.bot_username = bot_info.username
+        logger.info(f"Bot initialized: @{config.bot_username}")
+    except Exception as e:
+        logger.error(f"Error initializing bot: {e}", exc_info=True)
+        raise
+
 async def main():
-    config = Config()
-    bot = TelegramClient('bot_session', config.api_id, config.api_hash)
-    await bot.start(bot_token=config.bot_token)
+    try:
+        config = Config()
+        bot = TelegramClient('bot_session', config.api_id, config.api_hash)
+        await bot.start(bot_token=config.bot_token)
 
-    db = DatabaseManager(config)
+        db = DatabaseManager(config)
 
-    admin_handlers = AdminHandlers(bot, config, db)
-    super_admin_handlers = SuperAdminHandlers(bot, config, admin_handlers, db)
-    user_start = UserStart(bot, config, db)
-    start_handlers = StartHandlers(bot, config, db)
+        await init_bot(bot, config)
 
-    from telethon import events
-    bot.add_event_handler(start_handlers.handle_start, events.NewMessage(pattern='/start'))
+        admin_handlers = AdminHandlers(bot, config, db)
+        super_admin_handlers = SuperAdminHandlers(bot, config, admin_handlers, db)
+        user_start = UserStart(bot, config, db)
+        start_handlers = StartHandlers(bot, config, db)
 
-    logger.info("Bot started")
-    await bot.run_until_disconnected()
+        from telethon import events
+        bot.add_event_handler(start_handlers.handle_start, events.NewMessage(pattern='/start'))
+
+        logger.info("Bot started successfully")
+        await bot.run_until_disconnected()
+    except Exception as e:
+        logger.error(f"Error in main: {e}")
+        raise
 
 if __name__ == '__main__':
     asyncio.run(main())
