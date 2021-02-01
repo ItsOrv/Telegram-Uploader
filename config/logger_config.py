c95d2e49c3d4290b420a675f26e02a8b2bf433f5
ItsOrv <poriya.saw@gmail.com>
2021-02-01 11:49:25 +0330

    add proper logging

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,8 +1,41 @@
 import logging
+import os
+from colorama import Fore, Style, init
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+init(autoreset=True)
 
-logger = logging.getLogger(__name__)
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: Fore.CYAN,
+        logging.INFO: Fore.GREEN,
+        logging.WARNING: Fore.YELLOW,
+        logging.ERROR: Fore.RED,
+        logging.CRITICAL: Fore.RED + Style.BRIGHT,
+    }
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelno, '')
+        record.levelname = f"{color}{record.levelname}{Style.RESET_ALL}"
+        return super().format(record)
+
+log_level = getattr(logging, os.getenv('LOG_LEVEL', 'INFO').upper(), logging.INFO)
+
+logger = logging.getLogger('telegram_uploader')
+logger.setLevel(log_level)
+
+if not logger.handlers:
+    console = logging.StreamHandler()
+    console.setLevel(log_level)
+    console.setFormatter(ColoredFormatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    ))
+    logger.addHandler(console)
+
+    log_file = os.getenv('LOG_FILE', 'logs/bot.log')
+    os.makedirs(os.path.dirname(log_file) if os.path.dirname(log_file) else '.', exist_ok=True)
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(log_level)
+    fh.setFormatter(logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    ))
+    logger.addHandler(fh)
