a7113a8415930c41bb1b9959e9d3d2983460edce
ItsOrv <poriya.saw@gmail.com>
2021-02-18 14:09:47 +0330

    improve channel checker

--- utils/channel_checker.py
+++ utils/channel_checker.py
@@ -1,12 +1,12 @@
 from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import ChannelParticipant
+from telethon.tl.types import ChannelParticipant, ChannelParticipantCreator, ChannelParticipantAdmin
 from config.logger_config import logger
 
 async def check_channel_membership(bot, user_id, channel_id):
     try:
         result = await bot(GetParticipantRequest(channel_id, user_id))
         if result and result.participant:
-            return isinstance(result.participant, ChannelParticipant)
+            return isinstance(result.participant, (ChannelParticipant, ChannelParticipantAdmin, ChannelParticipantCreator))
         return False
     except Exception as e:
         logger.debug(f"channel check: user {user_id} not in {channel_id}: {e}")
