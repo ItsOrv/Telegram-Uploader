f70aeb040711bbb65313566403a6b1b7a5b73ac8
ItsOrv <poriya.saw@gmail.com>
2021-02-19 10:39:31 +0330

    constants for messages

--- config/constants.py
+++ config/constants.py
@@ -1,3 +1,20 @@
+from config.logger_config import logger
+
+class FileConstants:
+    NOT_FOUND = "فایل مورد نظر یافت نشد."
+    DELETE = type('obj', (object,), {'PENDING': "فایل در چند ثانیه دیگر حذف خواهد شد."})()
+
+class AdminConstants:
+    UPLOAD_REQUEST = {
+        'NEW': "درخواست آپلود جدید:\nکاربر: {user_id}\nیوزرنیم: @{username}"
+    }
+
+class SuperAdminConstants:
+    WELCOME = "پنل سوپر ادمین"
+
+class UserConstants:
+    ERROR = "خطایی رخ داد. لطفا دوباره تلاش کنید."
+
 class Constants:
     WELCOME_MESSAGE = "سلام! به ربات خوش آمدید."
     ADMIN_PANEL_MESSAGE = "پنل مدیریت"
@@ -6,6 +23,13 @@ class Constants:
     CANCEL_COMMAND = "لغو شد."
     SUPPORT_PROMPT = "پیام خود را بفرستید:"
     SUPPORT_MESSAGE_SENT = "پیام شما ارسال شد."
+    SET_GROUP_HELP = "ابتدا گروه اصلی و بکاپ را ست کنید."
+    SET_MAIN_GROUP_SUCCESS = "گروه اصلی با موفقیت ست شد."
+    SET_BACKUP_GROUP_SUCCESS = "گروه بکاپ با موفقیت ست شد."
 
     def __init__(self, config=None):
         self.config = config
+        self.file = FileConstants()
+        self.admin = AdminConstants()
+        self.super_admin = SuperAdminConstants()
+        self.user = UserConstants()
