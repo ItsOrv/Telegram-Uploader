cdb4b58f710936689254cede3ee3bea47d486168
ItsOrv <poriya.saw@gmail.com>
2021-02-20 16:14:03 +0330

    README cleanup

--- config/config.py
+++ config/config.py
@@ -4,13 +4,63 @@ from dotenv import load_dotenv
 from .logger_config import logger
 
 class Config:
-    def __init__(self):
+    """Configuration handler for Telegram Uploader.
+
+    Loads and validates environment variables required for the application.
+    """
+
+    def __init__(self) -> None:
+        """Initialize configuration from environment variables."""
+        logger.info("Initializing configuration")
         load_dotenv()
+        logger.debug("Environment variables loaded from .env file")
+        self._api_id = None
+        self._api_hash = None
+        self._bot_token = None
+        self._super_admin_id = None
+        self._delete_delay = None
+        self._db_config = {}
+        self._proxy = {}
+        self._session_file = None
+        self._log_file = None
+        self._max_file_size = None
+        self._max_send_count_per_user = None
+        self._max_send_count_per_group = None
+        self._allowed_file_types = []
+        self._required_channels = []
+        self._log_level = None
+        self.validate_config()
+        logger.info("Configuration initialized successfully")
+
+    def validate_config(self) -> None:
+        """Validate that all required configuration values are present."""
+        required_vars = [
+            'API_ID',
+            'API_HASH',
+            'BOT_TOKEN',
+            'SUPER_ADMIN_ID'
+        ]
+
+        for var in required_vars:
+            if not os.getenv(var):
+                raise ValueError(f"Missing required environment variable: {var}")
+
+        # Convert string values to appropriate types
         self._api_id = int(os.getenv('API_ID'))
         self._api_hash = os.getenv('API_HASH')
         self._bot_token = os.getenv('BOT_TOKEN')
         self._super_admin_id = int(os.getenv('SUPER_ADMIN_ID'))
-        self._delete_delay = int(os.getenv('DELETE_DELAY', 30))
+
+        # Proxy settings for Telegram
+        self._proxy = {
+            'proxy_type': 'socks5',  # or 'http' depending on your proxy
+            'addr': '127.0.0.1',     # your proxy address
+            'port': 1080,            # your proxy port
+            'username': os.getenv('PROXY_USERNAME', None),
+            'password': os.getenv('PROXY_PASSWORD', None)
+        }
+
+        # Database configuration
         self._db_config = {
             'host': os.getenv('MYSQL_HOST', 'localhost'),
             'port': int(os.getenv('MYSQL_PORT', 3306)),
@@ -18,87 +68,99 @@ class Config:
             'password': os.getenv('MYSQL_PASSWORD'),
             'database': os.getenv('MYSQL_DATABASE', 'telegram_uploader')
         }
+
+        # File configuration
         self._session_file = os.getenv('SESSION_FILE', 'bot_session')
         self._log_file = os.getenv('LOG_FILE', 'logs/bot.log')
-        self._max_file_size = int(os.getenv('MAX_FILE_SIZE', 52428800))
+        self._max_file_size = int(os.getenv('MAX_FILE_SIZE', 52428800))  # 50MB default
+
+        # App configuration
+        self._delete_delay = int(os.getenv('DELETE_DELAY', 30))
+        self._max_send_count_per_user = int(os.getenv('MAX_SEND_COUNT_PER_USER', 10))
+        self._max_send_count_per_group = int(os.getenv('MAX_SEND_COUNT_PER_GROUP', 1000))
         self._allowed_file_types = os.getenv('ALLOWED_FILE_TYPES', '').split(',')
         self._required_channels = os.getenv('REQUIRED_CHANNELS', '').split(',')
+
+        # Logging configuration
         self._log_level = os.getenv('LOG_LEVEL', 'INFO')
 
-    def validate_config(self):
-        required = ['API_ID', 'API_HASH', 'BOT_TOKEN', 'SUPER_ADMIN_ID']
-        for var in required:
-            if not os.getenv(var):
-                raise ValueError(f"Missing required env var: {var}")
-
     @property
-    def api_id(self):
+    def api_id(self) -> int:
         return self._api_id
 
     @property
-    def api_hash(self):
+    def api_hash(self) -> str:
         return self._api_hash
 
     @property
-    def bot_token(self):
+    def bot_token(self) -> str:
         return self._bot_token
 
     @property
-    def super_admin_id(self):
+    def super_admin_id(self) -> int:
         return self._super_admin_id
 
     @property
-    def delete_delay(self):
+    def delete_delay(self) -> int:
         return self._delete_delay
 
     @property
-    def mysql_host(self):
+    def mysql_host(self) -> str:
         return self._db_config['host']
 
     @property
-    def mysql_port(self):
+    def mysql_port(self) -> int:
         return self._db_config['port']
 
     @property
-    def mysql_user(self):
+    def mysql_user(self) -> str:
         return self._db_config['user']
 
     @property
-    def mysql_password(self):
+    def mysql_password(self) -> str:
         return self._db_config['password']
 
     @property
-    def mysql_database(self):
+    def mysql_database(self) -> str:
         return self._db_config['database']
 
     @property
-    def session_file(self):
+    def data_file(self) -> str:
+        """Remove this property as it's no longer needed"""
+        raise NotImplementedError("data_file is deprecated. Use MySQL database instead.")
+
+    @property
+    def proxy(self) -> dict:
+        return self._proxy
+
+    @property
+    def session_file(self) -> str:
         return self._session_file
 
     @property
-    def log_file(self):
+    def log_file(self) -> str:
         return self._log_file
 
     @property
-    def max_file_size(self):
+    def max_file_size(self) -> int:
         return self._max_file_size
 
     @property
-    def allowed_file_types(self):
+    def max_send_count_per_user(self) -> int:
+        return self._max_send_count_per_user
+
+    @property
+    def max_send_count_per_group(self) -> int:
+        return self._max_send_count_per_group
+
+    @property
+    def allowed_file_types(self) -> list:
         return self._allowed_file_types
 
     @property
-    def required_channels(self):
+    def required_channels(self) -> list:
         return self._required_channels
 
     @property
-    def log_level(self):
+    def log_level(self) -> str:
         return self._log_level
-
-    @property
-    def bot_username(self):
-        return getattr(self, '_bot_username', None)
-
-    @bot_username.setter
-    def bot_username(self, value):
-        self._bot_username = value
--- utils/save.py
+++ utils/save.py
@@ -1,38 +1,72 @@
-import hashlib
 import json
+import hashlib
+from config.db_manager import DatabaseManager
 from config.logger_config import logger
 
 class Save:
     def __init__(self, bot, config, db):
+        logger.info("Initializing Save utility")
         self.bot = bot
         self.config = config
         self.db = db
+        logger.debug("Save utility initialized successfully")
+
+    def get_file_caption(self, original_caption: str = None) -> str:
+        """Get complete caption with default text."""
+        default_caption = self.db.get_config('default_caption')
+        default_text = default_caption.get('value', '') if default_caption else ''
+
+        if original_caption:
+            return f"{original_caption}\n\n{default_text}"
+        return default_text
 
     async def save_file_to_group(self, message, user_id):
+        """Save file to group and database."""
         try:
             file = message.file
             admin_info = self.db.get_admin(user_id)
 
             if not admin_info or not admin_info.get('group_id'):
-                raise ValueError("group not set")
+                raise ValueError("گروه مورد نظر یافت نشد. لطفا ابتدا گروه را تنظیم کنید.")
 
             group_id = admin_info['group_id']
+            backup_group_id = admin_info.get('group_backup_id')
+
+            # Send file to primary group
             sent_message = await self.bot.send_file(group_id, file)
 
-            file_id = str(sent_message.id)
-            raw = f"{file_id}{group_id}{user_id}"
-            hash_file = hashlib.md5(raw.encode()).hexdigest()
-            return {
-                'file_id': file_id,
+            # Send to backup group if configured
+            if backup_group_id:
+                await self.bot.send_file(backup_group_id, file)
+
+            file_info = {
+                'file_id': str(sent_message.id),
                 'group_id': str(group_id),
-                'hash_file': hash_file,
                 'file_name': file.name,
                 'file_size': file.size,
                 'mime_type': file.mime_type,
-                'uploader_id': user_id
+                'uploader_id': user_id,
+                'download_link': f"https://t.me/{self.config.bot_username}?start={sent_message.id}"
             }
+
+            raw = f"{file_info['file_id']}{group_id}{user_id}"
+            hash_file = hashlib.md5(raw.encode()).hexdigest()
+            file_info['hash_file'] = hash_file
+
+            self.db.add_file(
+                file_id=file_info['file_id'],
+                user_id=user_id,
+                file_name=file_info['file_name'],
+                file_size=file_info['file_size'],
+                mime_type=file_info['mime_type'],
+                uploader_id=user_id
+            )
+
+            logger.info(f"File saved successfully: {hash_file}")
+            return file_info
+
         except Exception as e:
-            logger.error(f"save_file_to_group error: {e}", exc_info=True)
+            logger.error(f"Error saving file: {e}", exc_info=True)
             raise
 
     def get_file_info(self, file_hash):
